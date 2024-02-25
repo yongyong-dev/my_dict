dd344dd9d1dfb7fece5030534272239c36b40a58
Yongjin Kwon <dydwls159951@gmail.com>
2024-02-26 04:45:40 +0900

    Initial Commit

---
+++ myDictionary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myDictionary
+//
+//  Created by 권용진 on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myDictionary/myDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDictionaryApp.swift
+//  myDictionary
+//
+//  Created by 권용진 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct myDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
